9f0e520f06f6f8ffa9661c36b96c591d8704435a
Рина <kremis8@mail.ru>
2024-02-12 19:54:52 +0300

    Выполнение 10 главы методички. Пользовательские события, добовление кнопки удаления товара из корзины

--- Week1vue.js/js/main.js
+++ Week1vue.js/js/main.js
@@ -6,38 +6,45 @@ Vue.component('product', {
         }
     },
     template: `
-      <div class="product">
-        <div class="product-image">
-          <img :src="image" :alt="altText"/>
-        </div>
-        <div class="product-info">
-          <h1>{{ title }}</h1>
-          <p v-if="inStock">In Stock</p>
-          <p v-else style="text-decoration: line-through">Out of Stock</p>
+   <div class="product">
+    <div class="product-image">
+           <img :src="image" :alt="altText"/>
+       </div>
+
+       <div class="product-info">
+           <h1>{{ title }}</h1>
+           <p v-if="inStock">In stock</p>
+           <p v-else>Out of Stock</p>
+           <ul>
+               <li v-for="detail in details">{{ detail }}</li>
+           </ul>
           <p>Shipping: {{ shipping }}</p>
-          <product-details :details="details"></product-details>
-          <div
-              class="color-box"
-              v-for="variant in variants"
-              :key="variant.variantId"
-              :style="{ backgroundColor:variant.variantColor }"
-              @mouseover="updateProduct(variant.variantImage)">
-          </div>
-          <div class="cart">
-            <p>Cart({{ cart }})</p>
-          </div>
-          <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to cart</button>
-        </div>
-      </div>
+           <div
+                   class="color-box"
+                   v-for="(variant, index) in variants"
+                   :key="variant.variantId"
+                   :style="{ backgroundColor:variant.variantColor }"
+                   @mouseover="updateProduct(index)"
+           ></div>
+          
+           <button
+                   v-on:click="addToCart"
+                   :disabled="!inStock"
+                   :class="{ disabledButton: !inStock }"
+           >
+               Add to cart
+           </button>
+           <button @click="deleteFromCart">Delete</button>
+       
+       </div>
+   </div>
  `,
     data() {
         return {
             product: "Socks",
             brand: 'Vue Mastery',
             selectedVariant: 0,
-            image: "src/assets/vmSocks-green-onWhite.jpg",
             altText: "A pair of socks",
-            inStock: true,
             details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             variants: [
                 {
@@ -50,19 +57,28 @@ Vue.component('product', {
                     variantId: 2235,
                     variantColor: 'blue',
                     variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                    variantQuantity: 0
+                    variantQuantity: 3
                 }
             ],
-            cart: 0,
+            cart: 0
         }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
+        updateCart(id) {
+            this.cart.push(id);
+        },
+        deleteFromCart: function() {
+            this.$emit('delete-from-cart', this.variants[this.selectedVariant].variantId)
         }
+
     },
     computed: {
         title() {
@@ -71,7 +87,7 @@ Vue.component('product', {
         image() {
             return this.variants[this.selectedVariant].variantImage;
         },
-        inStock(){
+        inStock() {
             return this.variants[this.selectedVariant].variantQuantity
         },
         shipping() {
@@ -81,29 +97,26 @@ Vue.component('product', {
                 return 2.99
             }
         }
-
     }
-})
-Vue.component('product-details', {
-    props: {
-        details: {
-            type: Array,
-            required: true
-        }
-    },
-    template: `
-        <ul>
-            <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-    `
 })
 
 let app = new Vue({
     el: '#app',
     data: {
-        premium: true
+        premium: true,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
+        deleteItem(id) {
+            for (let i = this.cart.length - 1; i >= 0; i--) {
+                if (this.cart[i] === id) {
+                    this.cart.splice(i, 1);
+                }
+            }
+        }
     }
 })
-
-
 
